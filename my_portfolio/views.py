f1ca1a6a9a073fb51af46ecd67c68a02768d652a
Mostafa Rayhan <mostafarayhan57@gmail.com>
2023-02-15 01:48:03 +0600

    contact

--- my_portfolio/views.py
+++ my_portfolio/views.py
@@ -5,18 +5,6 @@ from .models import *
 # Create your views here.
 
 def index(request):
-    if request.method=="POST":
-        contact=Contact()
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        contact.name = name
-        contact.email = email
-        contact.subject = subject
-        contact.message = message
-        contact.save()
-        return render(request, 'index.html')
     # all = All.objects.all()
     photoshop = Photoshop.objects.all()
     art = Art.objects.all()
@@ -27,15 +15,28 @@ def index(request):
         'arts': art,
         'webs': web
     }
+
     return render(request, 'index.html', context)
 
 
-# def form_submission(request):
-#     name = request.POST['name']
-#     email = request.POST['email']
-#     subject = request.POST['subject']
-#     message = request.POST['message']
-#
-#     form = Contact.objects.create(name=name, email=email, subject=subject, message=message)
-#     form.save()
+def form_submission(request):
+    if request.method == "GET":
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        contact.name = name
+        contact.email = email
+        contact.subject = subject
+        contact.message = message
+        contact.save()
 #     return render(request, 'index.html')
+#         name = request.POST['name']
+#         email = request.POST['email']
+#         subject = request.POST['subject']
+#         message = request.POST['message']
+#
+#         form = Contact.objects.create(name=name, email=email, subject=subject, message=message)
+#         form.save()
+    return render(request, 'index.html')
